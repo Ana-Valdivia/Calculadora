a1338193922141c4c7e6ce2e70a1d5fe6b47152c
Ana-Valdivia <ana.raquel1291@hotmail.com>
2019-06-27 20:12:11 -0300

    [FEAT]: Upload de arquivos calculadora C#.

---
+++ exemplinho/Calculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplinho
+{
+    class Calculadora
+    {
+        //Atributos sempre são privados.
+        private double num1;
+        private double num2;
+
+        //Encapsulamento.
+        public double Num1 { get => num1; set => num1 = value; }
+        public double Num2 { get => num2; set => num2 = value; }
+
+        //Métodos.
+
+        public double fazerSoma() //Método de somar
+        {
+            return(this.num1 + this.num2);
+        }
+        public double fazerSub() //Método de subtrair
+        {
+            {
+            return (this.num1 - this.num2);
+        }
+
+    }
+}
---
+++ exemplinho/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exemplinho
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Instanciar.
+            //nomedoobjeto = colocar o nome do objeto.
+
+            Calculadora calc = new Calculadora();
+            calc.Num1 = Convert.ToDouble(textBox1.Text);
+            calc.Num2 = Convert.ToDouble(textBox2.Text);
+            textBox3.Text = Convert.ToString(calc.fazerSoma());
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Calculadora calc = new Calculadora();
+            calc.Num1 = Convert.ToDouble(textBox1.Text);
+            calc.Num2 = Convert.ToDouble(textBox2.Text);
+            textBox3.Text = Convert.ToString(calc.fazerSub());
+
+        }
+    }
+}
